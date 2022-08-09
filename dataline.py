8b589de3b4c4820203c70978b15bcf8ce65bbaaf
Hisham0814 <95046829+Hisham0814@users.noreply.github.com>
2022-08-09 22:18:38 +0530

    Update dataline.py

--- dataline.py
+++ dataline.py
@@ -9,11 +9,7 @@ var = pd.read_excel(file1)
 x = list(var['X values'])
 y = list(var['Y values'])
 
-
 plt.plot(x,y,color='green',marker='o',markerfacecolor='blue',markersize=8)
-
 
 plt.savefig('C:\\plot\\myplot.png')
 plt.show()
-#"C:\Users\hisha\AppData\Local\Microsoft\CLR_v4.0\UsageLogs\python.exe.log"
-#"C:\Users\hisha\AppData\Local\Microsoft\WindowsApps\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe\python.exe"
