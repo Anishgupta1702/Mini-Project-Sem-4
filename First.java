7ebff191c1899d2ac1aa7ee09930dbba2b2a787e
Hisham0814 <95046829+Hisham0814@users.noreply.github.com>
2022-08-09 20:45:09 +0530

    Update First.java

--- First.java
+++ First.java
@@ -1,4 +1,5 @@
 import java.awt.Choice;
+import java.awt.Component;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -6,11 +7,6 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.UIManager;
-import javax.swing.border.EtchedBorder;
-
-//import test.MainFrame;
-//import test.Second;
 
 public class First extends JPanel {
 
@@ -19,10 +15,12 @@ public class First extends JPanel {
 	 */
 	public First() {
 		
+		setSize(530, 400);
+		
 		setLayout(null);
 				
 		Choice c = new Choice();
-		c.setBounds(262, 158, 178, 20);
+		c.setBounds(262, 158, 160, 20);
 		add(c);
 		c.add("User input");
 		c.add("Default data");
@@ -30,13 +28,13 @@ public class First extends JPanel {
 				
 		JLabel lblNewLabel = new JLabel("Select your option:\r\n");
 		lblNewLabel.setFont(new Font("Times New Roman", Font.PLAIN, 20));
-		lblNewLabel.setBounds(106, 158, 150, 20);
+		lblNewLabel.setBounds(100, 158, 150, 20);
 		add(lblNewLabel);
 			
-		JButton btnNewButton = new JButton("OK");
-		btnNewButton.addActionListener(new ActionListener() {
+		JButton button = new JButton("OK");
+		button.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if (c.getSelectedItem()=="Default data" && e.getSource() == btnNewButton) {
+				if (c.getSelectedItem()=="Default data" && e.getSource() == button) {
 					MainFrame.main_panel.invalidate();
 					MainFrame.main_panel.removeAll();
 					Second frame2 = new Second();
@@ -44,10 +42,18 @@ public class First extends JPanel {
 					MainFrame.main_panel.revalidate();
 					MainFrame.main_panel.repaint();
 				}
+				else if(c.getSelectedItem()=="User input" && e.getSource() == button){
+					MainFrame.main_panel.invalidate();
+					MainFrame.main_panel.removeAll();
+					UserInput frameUser = new UserInput();
+					MainFrame.main_panel.add(frameUser);
+					MainFrame.main_panel.revalidate();
+					MainFrame.main_panel.repaint();
+				}
 			}
 		});
-		btnNewButton.setBounds(235, 206, 52, 23);
-		add(btnNewButton);
+		button.setBounds(240, 220, 52, 23);
+		add(button);
 	}
 
 }
